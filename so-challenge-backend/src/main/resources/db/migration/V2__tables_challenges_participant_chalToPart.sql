68523f50db43bb7173dfa9a1b93924d095801f58
ext_herbert.wirnsber <herbert.wirnsberger@gepardec.com>
2019-08-19 11:55:17 +0200

    added flyway to have automatic db migrations

---
+++ so-challenge-backend/src/main/resources/db/migration/V2__tables_challenges_participant_chalToPart.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS challenge (
+	id serial,
+	title varchar(500),
+	beginDate date,
+	endDate date,
+	award1 varchar(500),
+	PRIMARY KEY( id )
+);
+
+CREATE TABLE IF NOT EXISTS participant (
+    profileId bigint,
+    link varchar(500),
+    username varchar(300),
+    PRIMARY KEY( profileId )
+);
+
+CREATE TABLE IF NOT EXISTS challenge_participant (
+    challenge_id int REFERENCES challenge (id) ON UPDATE CASCADE ON DELETE CASCADE ,
+    participant_id int REFERENCES participant (profileId) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT challenge_participant_pkey PRIMARY KEY (challenge_id, participant_id) -- explicit pk
+);
